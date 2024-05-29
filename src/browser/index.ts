f33aa93154d3576f05f0a86f5ece43620921eafa
Solomon Astley <solastley@gmail.com>
2024-05-28 17:39:13 -0700

    Buffer calls to unify client in global context before script is loaded (#3)
    * buffer calls to unify client in global context before script is loaded
    
    * add unit tests
    
    * update dist

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,6 +1,22 @@
 import UnifyIntentClient from 'client';
 
+declare global {
+  interface Window {
+    unify?: UnifyIntentClient;
+  }
+}
+
+/**
+ * Initializes the `UnifyIntentClient` and flushes pre-made method calls
+ * from the global context if there are any.
+ */
 export const initBrowser = function () {
+  // If the client has already been initialized, do nothing
+  if (window.unify && !Array.isArray(window.unify)) {
+    return;
+  }
+
+  // Get public API key from the script tag
   const scriptTag = document.getElementById('unifytag');
   const writeKey =
     scriptTag?.getAttribute('data-write-key') ??
@@ -8,10 +24,55 @@ export const initBrowser = function () {
 
   if (!writeKey) return;
 
+  // Instantiate the Unify client
   const unify = new UnifyIntentClient(writeKey, {
     autoPage: true,
     autoIdentify: true,
   });
+
+  // Flush method calls which were made before this script loaded
+  flushUnifyQueue(unify);
+
+  // Re-assign the global Unify client object so future method calls
+  // will go straight to the newly instantiated client
+  window.unify = unify;
 };
+
+/**
+ * It's possible that client code will execute functions on the global
+ * `UnifyIntentClient` object before it is actually loaded and instantiated
+ * because this code is loaded asynchronously by the client.
+ *
+ * Until `flushUnifyQueue` is called, `window.unify` is set to an array of queued
+ * method calls, which are themselves each represented by an array. The first
+ * element of each of these "method call subarrays" is the method name to call,
+ * and the rest of the elements are the arguments to pass to that method.
+ *
+ * Once the Unify intent script is loaded and the `UnifyIntentClient` has
+ * been instantiated, this function is called to flush the queued method
+ * calls on the existing `window.unify` array if there are any to flush. It
+ * iterates over each queued method call and applies that method and its
+ * arguments to the newly instantiated `UnifyIntentClient`.
+ *
+ * @param unify - the `UnifyIntentClient` to apply method calls to
+ */
+function flushUnifyQueue(unify: UnifyIntentClient) {
+  const queue: [string, ...unknown[]][] = Array.isArray(window.unify)
+    ? [...window.unify]
+    : [];
+
+  queue.forEach(([method, ...args]) => {
+    if (typeof unify[method as keyof UnifyIntentClient] === 'function') {
+      try {
+        // @ts-expect-error the type of the args is unknown at this point
+        unify[method as keyof UnifyIntentClient].call(unify, ...args);
+      } catch (error) {
+        // Swallow errors so client is not potentially affected, this
+        // should ideally never happen.
+        console.warn(error);
+      }
+    }
+  });
+}
 
 initBrowser();
---
+++ src/tests/browser/browser.unit.test.ts
@@ -0,0 +1,54 @@
+import { initBrowser } from 'browser';
+import UnifyIntentClient from 'client';
+import { mock, mockReset } from 'jest-mock-extended';
+
+import { TEST_WRITE_KEY } from 'tests/mocks/data';
+
+const mockIntentClient = mock(UnifyIntentClient.prototype);
+jest.mock('client', () => ({
+  ...jest.requireActual('client'),
+  __esModule: true,
+  default: jest.fn().mockImplementation(() => mockIntentClient),
+}));
+
+const MOCK_UNIFY_TAG_WRITE_KEY = `
+    <script id="unifytag" data-write-key="${TEST_WRITE_KEY}"></script>
+`;
+
+const MOCK_UNIFY_TAG_API_KEY = `
+    <script id="unifytag" data-api-key="${TEST_WRITE_KEY}"></script>
+`;
+
+describe('Browser', () => {
+  beforeEach(() => {
+    document.body.innerHTML = MOCK_UNIFY_TAG_WRITE_KEY;
+    window.unify = undefined;
+    mockReset(mockIntentClient);
+  });
+
+  describe('initBrowser', () => {
+    it('initializes a UnifyIntentClient on the window', () => {
+      expect(window.unify).toBeFalsy();
+      initBrowser();
+      expect(window.unify).toBeTruthy();
+    });
+
+    it('falls back to data-api-key for legacy scripts', () => {
+      document.body.innerHTML = MOCK_UNIFY_TAG_API_KEY;
+      expect(window.unify).toBeFalsy();
+      initBrowser();
+      expect(window.unify).toBeTruthy();
+    });
+
+    it('clears methods in the queue', () => {
+      // @ts-expect-error
+      window.unify = [['page'], ['identify', 'solomon@unifygtm.com']];
+      initBrowser();
+      expect(mockIntentClient.page).toHaveBeenCalledTimes(1);
+      expect(mockIntentClient.identify).toHaveBeenCalledWith(
+        'solomon@unifygtm.com',
+      );
+      expect(window.unify).toBeTruthy();
+    });
+  });
+});
