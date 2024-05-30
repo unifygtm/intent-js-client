87e954153e37b3fcd56707c64875dcc324be51e8
Solomon Astley <solastley@gmail.com>
2024-05-30 14:44:53 -0700

    attempt to fix type exports

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,6 @@
 import UnifyIntentClient from './unify-intent-client';
+import { UnifyIntentClientConfig as ClientConfig } from '../types';
 
-export { UnifyIntentClientConfig } from '../types';
+export type UnifyIntentClientConfig = ClientConfig;
 
 export default UnifyIntentClient;
