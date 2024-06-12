c79730665a17e4a7b0114e7733fd686c28d1df26
Solomon Astley <solastley@gmail.com>
2024-06-12 14:04:47 -0400

    attempt to fix types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="@types/js-cookie" />
+/// <reference types="@types/uuid" />
+/// <reference types="user-agent-data-types" />
+
 export { UnifyIntentClientConfig } from './types';
 
 export { default as UnifyIntentClient } from './client/unify-intent-client';
